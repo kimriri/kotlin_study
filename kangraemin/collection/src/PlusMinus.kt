6d8f1db860b04e572131442323c50e9b306c94d1
kangraemin <remin1994@gmail.com>
2021-02-25 22:01:15 +0900

    Add sample code for plus / minus operator

---
+++ kangraemin/collection/src/PlusMinus.kt
@@ -0,0 +1,31 @@
+fun main() {
+
+}
+
+fun plusMinus() {
+    val numbers = listOf("one", "two", "three", "four")
+
+    val plusList = numbers + "five"
+    val minusList = numbers - listOf("three", "four")
+    println(plusList)
+    println(minusList)
+}
+
+fun augmentedAssignment() {
+    val valNumberCollection = listOf("one", "two", "three", "four")
+    // Compile error
+//    valNumberCollection += "five"
+//    valNumberCollection -= listOf("three", "four")
+//    valNumberCollection -= "four"
+
+    // mutableList로 바꿀 것을 권장, + / - operator 자체가 재할당 하는 형식이기 때문
+    var varNumberCollection = listOf("one", "two", "three", "four")
+    varNumberCollection += "five"
+    varNumberCollection -= listOf("three", "four")
+    varNumberCollection -= "four"
+
+    val valNumberMutableCollection = mutableListOf("one", "two", "three", "four")
+    valNumberMutableCollection += "five"
+    valNumberMutableCollection -= listOf("three", "four")
+    valNumberMutableCollection -= "four"
+}
